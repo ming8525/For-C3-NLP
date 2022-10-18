6db3515be19d16b38105def33ed6e734822d7c57
mng <mng.han@foxmail.com>
2022-10-18 09:51:11 +0800

    build: init repo

---
+++ packages/widgets/auto-line/src/config.ts
@@ -0,0 +1,6 @@
+import { ImmutableObject } from 'seamless-immutable'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Config {}
+
+export type IMConfig = ImmutableObject<Config>
---
+++ packages/widgets/auto-line/src/runtime/widget.tsx
@@ -0,0 +1,19 @@
+/** @jsx jsx */
+import { React, AllWidgetProps, jsx } from 'jimu-core'
+import { IMConfig } from '../config'
+
+export default class Widget extends React.PureComponent<AllWidgetProps<IMConfig>, any> {
+  constructor (props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <div className="widget-demo jimu-widget m-2">
+        <p>Autoline Widget</p>
+      </div>
+    )
+  }
+}
